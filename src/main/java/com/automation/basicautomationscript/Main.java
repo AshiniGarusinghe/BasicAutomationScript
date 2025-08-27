ddddc446493371ad6909979e6275a77c5e9cf30f
AshiniGarusinghe28 <ashinigarusinghe740@gmail.com>
2025-08-27 06:43:20 +0530

    update Main.java file

--- src/main/java/com/automation/basicautomationscript/Main.java
+++ src/main/java/com/automation/basicautomationscript/Main.java
@@ -1,17 +1,28 @@
 package com.automation.basicautomationscript;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.ebay.com/");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("gh-ac")).click();
+        driver.findElement(By.id("gh-ac")).clear();
+        driver.findElement(By.id("gh-ac")).sendKeys("iphone");
+
+
+        new Select(driver.findElement(By.id("gh-cat"))).selectByVisibleText("Cell Phones & Accessories");
+
+        driver.findElement(By.xpath("//span[text()='Search']")).click();
+        Thread.sleep(2000);
+
+
     }
 }
