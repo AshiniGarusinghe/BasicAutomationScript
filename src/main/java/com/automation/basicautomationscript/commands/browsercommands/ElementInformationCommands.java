fe509faacca4d3643e1afe7279fefc9e699d9e34
AshiniGarusinghe28 <ashinigarusinghe740@gmail.com>
2025-09-20 17:43:02 +0530

    practice for basic element information commands

--- src/main/java/com/automation/basicautomationscript/commands/browsercommands/ElementInformationCommands.java
+++ src/main/java/com/automation/basicautomationscript/commands/browsercommands/ElementInformationCommands.java
@@ -7,8 +7,7 @@ import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
-
-public class ElementInteractionCommands {
+public class ElementInformationCommands {
     public static void  main(String[]args) throws InterruptedException{
         WebDriver driver = WebDriverManager.chromedriver().create();
         driver.manage().window().maximize();
@@ -16,9 +15,15 @@ public class ElementInteractionCommands {
         driver.findElement(By.name("q")).sendKeys("iphone shops in sri lanka");
         driver.findElement(By.id("search_icon")).click();
         Thread.sleep(3000);
-        List<WebElement> elements = driver.findElements(By.cssSelector("[class='b_maglistcard'] h2"));
-                for(WebElement element : elements){
-                    System.out.println(element.getText());
+        List<WebElement> elements = driver.findElements(By.cssSelector("[class='bm_details_overlay'] h2"));
+        List<WebElement> elementsLinks = driver.findElements(By.cssSelector("[class='bm_details_overlay'] a"));
+        if(elements.size() == elementsLinks.size()){
+            for(int i = 0; i< elements.size(); i++){
+                System.out.println(elements.get(i).getText());
+                System.out.println(elementsLinks.get(i).getAttribute("href"));
+            }
+
+
         }
     }
 }
